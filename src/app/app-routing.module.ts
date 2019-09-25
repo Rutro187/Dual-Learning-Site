03e8ded7e971f9317f0828c8b5f083408899a9fa
Ben Eisenberger <ben.eisenberger@gmail.com>
2019-09-25 13:39:04 -0500

    styled admin component and others

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,16 +9,27 @@ import { DisplayQuizComponent } from './display-quiz/display-quiz.component';
 import { UserGuard } from './guards/user.guard';
 import { AdminGuard } from './guards/admin.guard';
 import { LoggedInGuard } from './guards/logged-in.guard';
+import { AdminComponent } from './admin/admin.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent, canActivate: [UserGuard]},
-  { path: 'register', component: RegisterFormComponent, canActivate: [UserGuard] },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [AdminGuard] },
-  { path: 'quiz_form', component: QuizFormComponent, canActivate: [AdminGuard] },
-  { path: 'quiz_guard', component: QuizGuardComponent, canActivate: [LoggedInGuard]},
-  { path: 'take_quiz',  component: DisplayQuizComponent, canActivate: [LoggedInGuard]}
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterFormComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'quiz_form', component: QuizFormComponent },
+  { path: 'quiz_guard', component: QuizGuardComponent},
+  { path: 'take_quiz',  component: DisplayQuizComponent },
+  { path: 'admin', component: AdminComponent }
+  // { path: '', redirectTo: '/login', pathMatch: 'full' },
+  // { path: 'login', component: LoginComponent, canActivate: [UserGuard]},
+  // { path: 'register', component: RegisterFormComponent, canActivate: [UserGuard] },
+  // { path: 'dashboard', component: DashboardComponent, canActivate: [AdminGuard] },
+  // { path: 'quiz_form', component: QuizFormComponent, canActivate: [AdminGuard] },
+  // { path: 'quiz_guard', component: QuizGuardComponent, canActivate: [LoggedInGuard]},
+  // { path: 'take_quiz',  component: DisplayQuizComponent, canActivate: [LoggedInGuard]}
+  // { path: 'admin', component: AdminComponent }
+
 ];
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -44,6 +44,7 @@ import { QuizResultsComponent } from './quiz-results/quiz-results.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterFormComponent } from './register-form/register-form.component';
+import { AdminComponent } from './admin/admin.component';
 
 
 
@@ -58,7 +59,8 @@ import { RegisterFormComponent } from './register-form/register-form.component';
     NavBarComponent,
     LoginComponent,
     RegisterFormComponent,
-    QuizGuardComponent
+    QuizGuardComponent,
+    AdminComponent
   ],
   imports: [
     FormsModule,
