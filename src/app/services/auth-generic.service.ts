501d95a9258c5538b80dc85f9a656043a776b899
Rutro187 <mjohnson@esomaha.com>
2019-09-26 12:04:40 -0500

    firebase

--- src/app/services/auth-generic.service.ts
+++ src/app/services/auth-generic.service.ts
@@ -1,31 +1,31 @@
 import { Injectable } from '@angular/core';
 // import { AngularFireAuth } from '@angular/fire/auth';?
 // import * as firebase from 'firebase/app';
-import { firebase } from 
 import { Router } from '@angular/router'
-
+import {AngularFireAuth } from '@angular/fire/auth'
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGenericService {
+  userData: Observable<firebase.User>;
 
+  // this.userData = angularFireAuth.authState; this was bello in the brackets 
+  constructor(private router: Router, private angularFireAuth: AngularFireAuth) {}
 
-
-  constructor(private router: Router) { }
-
-  doLogin(value){
-    return new Promise<any>((resolve, reject) => {
-      firebase.auth().signInWithEmailAndPassword(value.email, value.password)
-      .then(res => {
-        resolve(res);
-        if (res.user.emailVerified !== true) {
-          this.SendVerificationMail();
-          window.alert('Please validate your email address. Kindly check your inbox.');
-          this.router.navigate(['/login']);
-          }
-      }, err => reject(err))
-    })
-  }
+  // doLogin(value){
+  //   return new Promise<any>((resolve, reject) => {
+  //     firebase.auth().signInWithEmailAndPassword(value.email, value.password)
+  //     .then(res => {
+  //       resolve(res);
+  //       if (res.user.emailVerified !== true) {
+  //         this.SendVerificationMail();
+  //         window.alert('Please validate your email address. Kindly check your inbox.');
+  //         this.router.navigate(['/login']);
+  //         }
+  //     }, err => reject(err))
+  //   })
+  // }
 
 //   SendVerificationMail() {
 //     return this.afAuth.auth.currentUser.sendEmailVerification()
@@ -46,5 +46,15 @@ export class AuthGenericService {
 //       })
 //     })
 //   }
-
+signup(email: string, password: string) {
+  this.angularFireAuth
+  .auth
+  .createUserWithEmailAndPassword(email, password)
+  .then(res => {
+    console.log('Successfully signed up!', res);
+  })
+  .catch(error => {
+    console.log('Something is wrong:', error.message);
+  });
+}
 }
