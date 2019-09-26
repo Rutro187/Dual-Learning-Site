d14a1acca87bb22f38c80d3c31bee5663e10a233
Graham <greysnaa@gmail.com>
2019-09-26 13:54:31 -0500

    starting rewrite

--- src/app/quiz-form/quiz-form.component.ts
+++ src/app/quiz-form/quiz-form.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FormBuilder, Validators, FormControl, FormGroup, NgForm } from '@angular/forms';
 import { QuizServiceService, Quiz, Questions } from '../services/quiz-service.service';
 import { Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { UserServiceService } from '../services/user-service.service';
-import { NavbarService } from '../navbar.service';
+
+// export class RadioNgModelExample {
 
 
 @Component({
@@ -13,25 +14,19 @@ import { NavbarService } from '../navbar.service';
   styleUrls: ['./quiz-form.component.scss']
 })
 export class QuizFormComponent implements OnInit {
-  quizTitle;
-  quizDescription;
-  quizInstructions;
-  quizQuestions;
-  correctAnswers;
-  currentQuizToken;
-  quizToken;
+  answers: Array<string>; // Array of answers, entered by creator
+  correct: number; // index of answers array with correct answer
+  type: string; // type of question, multiChoice, trueFalse
+  points: number; // number of points question is worth
+  creator: string; // ID of who is making the quiz
+  title: string; // title of quiz
+  desc: string; // description of quiz
 
 
-  constructor( private quizService: QuizServiceService, private router: Router, private userServ: UserServiceService, public navbar: NavbarService) { }
+  constructor(private quizService: QuizServiceService, private router: Router, private userServ: UserServiceService) { }
 
   questions: Array<Object> = [
-    {
-      type: "multi",
-      prompt: "",
-      choices: [],
-      correct: "",
-
-    }
+    {}
   ]
 
   types: string[] = ["multi"]
@@ -42,7 +37,7 @@ export class QuizFormComponent implements OnInit {
 
   trackByFn(index: any, item: any) {
     return index;
- }
+  }
   removeQuestion(idx) {
     this.questions.splice(idx, 1)
   }
@@ -51,27 +46,32 @@ export class QuizFormComponent implements OnInit {
     this.questions.push({
       type: "multi",
       prompt: "",
-      choices: [],
-      correct: "",
-
+      choices: ["", ""],
+      correct: [true, false],
     })
   }
 
+
+  onChange(event) {
+    this.selectedType = event.target.value;
+  }
+
+
   removeChoice(question, cidx) {
     console.log(cidx);
-   question['choices'].splice(cidx, 1);
-   console.log(this.questions);
+    question['choices'].splice(cidx, 1);
+    console.log(this.questions);
   }
 
   addChoice(val) {
-    console.log(this.questions[val]);
+    // console.log(this.questions[val]);
     this.questions[val]['choices'].push('');
 
   }
-hideCreateQuiz(){
-  let sideBar = document.getElementById('sideBarContent');
-sideBar.remove();
-}
+  hideCreateQuiz() {
+    let sideBar = document.getElementById('sideBarContent');
+    sideBar.remove();
+  }
 
   showThankYou() {
     document.getElementById('thankYou').id = 'visible';
@@ -81,12 +81,10 @@ sideBar.remove();
   }
   quizFormSubmit() {
     let quiz: any = {
-      title: this.quizTitle,
-      description: this.quizDescription,
-      instrutions: this.quizInstructions,
+      title: this.title,
+      description: this.desc,
+      creator: this.creator, // need to pull this data from instance
       questions: this.questions,
-      // creatorId: this.userServ.logger,
-      creatorId: 20
     };
     console.log(quiz);
     this.quizService.postQuiz(quiz).subscribe(res => {
@@ -95,10 +93,11 @@ sideBar.remove();
       this.currentQuizToken = res['token'];
     });
     this.hideCreateQuiz();
-this.showThankYou();
+    this.showThankYou();
   }
 
 
   ngOnInit() {
+
   }
 }
