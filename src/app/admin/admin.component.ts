f9ed230bbfc717d8bc35d911b40f5b1fae3dc69f
Kile Donley <kiledonley@gmail.com>
2019-09-27 15:50:56 -0500

    merge done

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { AuthGenericService } from '../services/auth-generic.service';
-
+import {User} from '../Interfaces/users';
+import {MatTableDataSource} from '@angular/material/table';
 @Component({
-  selector: 'app-admin',
-  templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.scss']
+ selector: 'app-admin',
+ templateUrl: './admin.component.html',
+ styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
-  displayedColumns = ['displayname', 'email', 'permission', ]
-  constructor() {}
-  
-
-  ngOnInit() {}
-  }
+ displayedColumns = ['displayname', 'email', 'permission', ]
+ constructor() {
+}
+users: User[] = [
+ {displayname: 'Kile Dude', email: 'kile@kile.com', permission: 'Admin'},
+ {displayname: 'Ben Guy', email: 'Ben@kile.com', permission: 'User'},
+ {displayname: 'Carlos Man', email: 'Carlos@kile.com', permission: 'Teacher'},
+];
+dataSource = new MatTableDataSource(this.users);
+applyFilter(filterValue: string) {
+ this.dataSource.filter = filterValue.trim().toLowerCase();
+}
+ngOnInit() {
+}
+}
